3affc759bdfe11912e590d21b7fdd4890a98dc62
Simran Kumari <simrankumari@crio-users.in>
2022-06-13 03:44:13 +0000

    Update main methods with run App.java

--- SeleniumWebActions/lib/src/main/java/seleniumWebActions/App.java
+++ SeleniumWebActions/lib/src/main/java/seleniumWebActions/App.java
@@ -20,42 +20,42 @@ public class App {
 
         // TODO1 - Uncomment for Milestone 3 Activity - Add the RemoteWebDriver config
         BrowserInit browserInit = new BrowserInit();
-        browserInit.main(null);
+        browserInit.run();
 
         // TODO2 - Uncomment for Milestone 4 Activity - Automate BrowserActions
         // BrowserActions browserActions = new BrowserActions();
-        // browserActions.main(null);
+        // browserActions.run();
 
         // TODO3 - Uncomment for Milestone 6 Activity - Automate NavigationCommands
         // NavigationCommands navigationCommands = new NavigationCommands();
-        // navigationCommands.main(null);
+        // navigationCommands.run();
 
         // TODO4 - Uncomment for Milestone 7 Activity - findElement (FindLocators.java)
         // FindLocators findLocators = new FindLocators();
-        // findLocators.main(null);
+        // findLocators.run();
 
         // TODO5 - Uncomment for Milestone 7 Activity - findElements (CountNoOfProducts.java)
         // CountNoOfProducts countNoOfProducts = new CountNoOfProducts();
-        // countNoOfProducts.main(null);
+        // countNoOfProducts.run();
 
         // TODO6 - Uncomment for Milestone 7 Activity - findElementByXX (FindLocatorsUsingFindElementBy.java)
         // FindLocatorsUsingFindElementBy findLocatorsUsingFindElementBy = new FindLocatorsUsingFindElementBy();
-        // findLocatorsUsingFindElementBy.main(null);
+        // findLocatorsUsingFindElementBy.run();
 
         // TODO7 - Uncomment for Milestone 7 Activity - findElementsByXX (ProductCountUsingFindElementsBy.java)
         // ProductCountUsingFindElementsBy productCountUsingFindElementsBy = new ProductCountUsingFindElementsBy();
-        // productCountUsingFindElementsBy.main(null);
+        // productCountUsingFindElementsBy.run();
         
         // TODO8 - Uncomment for Milestone 8 Activity - Web Elements Action
         // WebElementAction webElementAction = new WebElementAction();
-        // webElementAction.main(null);
+        // webElementAction.run();
 
         // TODO9 - Uncomment for Milestone 9 Activity - WebDriverException
         // WebDriverException webDriverException = new WebDriverException();
-        // webDriverException.main(null);
+        // webDriverException.run();
 
         // TODO10 - Uncomment for Milestone 9 Activity - WebElementException
         // WebElementException webElementException = new WebElementException();
-        // webElementException.main(null);
+        // webElementException.run();
     }
 }
